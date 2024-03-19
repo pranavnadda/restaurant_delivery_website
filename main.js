baca9c7329d6d6ae3978a3a6442af98cb698de63
pranavnadda <106960271+pranavnadda@users.noreply.github.com>
2024-03-19 19:33:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", () => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 800,
+    easing: "ease-in-out",
+    reset: true
+};
+
+ScrollReveal().reveal(".header__image img", { ...scrollRevealOption, origin: "right" });
+ScrollReveal().reveal(".header__content h1", { ...scrollRevealOption, delay: 500 });
+ScrollReveal().reveal(".header__content .section__description", { ...scrollRevealOption, delay: 100 });
+ScrollReveal().reveal(".header__btn", { ...scrollRevealOption, delay: 150 });
+ScrollReveal().reveal(".explore__image img", { ...scrollRevealOption, origin: "left" });
+ScrollReveal().reveal(".explore__content .section__header", { ...scrollRevealOption, delay: 500 });
+ScrollReveal().reveal(".explore__content .section__description", { ...scrollRevealOption, delay: 100 });
+ScrollReveal().reveal(".explore__btn", { ...scrollRevealOption, delay: 150 });
+ScrollReveal().reveal(".banner__card", { ...scrollRevealOption, interval: 500 });
+ScrollReveal().reveal(".chef__image img", { ...scrollRevealOption, origin: "right" });
+ScrollReveal().reveal(".chef__content .section__header", { ...scrollRevealOption, delay: 500 });
+ScrollReveal().reveal(".chef__content .section__description", { ...scrollRevealOption, delay: 100 });
+ScrollReveal().reveal(".chef__list li", { ...scrollRevealOption, delay: 150, interval: 500 });
+
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
